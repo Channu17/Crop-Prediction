581dac27fd7f1911210961bab68cc30e1433f65d
Channu17 <channudambal2004@gmail.com>
2025-01-17 12:56:03 +0530

    soil classification api finished

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
 import pickle
 from pydantic import BaseModel
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import cv2
 
 app = FastAPI()
 
@@ -14,6 +19,16 @@ with open('models/label_encoder.pkl', 'rb') as f:
 with open('models/scalar.pkl', 'rb') as f:
     scalar = pickle.load(f)
     
+soil_classifier = load_model('models/soilClassification.keras')
+
+with open('models/label_encoder_soil_classification.pkl', 'rb') as f:
+    label_encoder_sc = pickle.load(f)
+    
+def preprocess_image(image):
+    img_array = np.array(image) / 255.0
+    img_array = tf.image.resize(img_array, (224, 224))  
+    img_array = tf.expand_dims(img_array, axis=0)  
+    return img_array
 
 class Values(BaseModel):
     N : float
@@ -26,17 +41,57 @@ class Values(BaseModel):
 
 @app.post('/prediction')
 def prediction(values:Values):
-    input_data = pd.DataFrame([{
-        "N": values.N,
-        "P": values.P,
-        "K": values.K,
-        "temperature": values.temperature,
-        "humidity": values.humidity,
-        "ph": values.ph,
-        "rainfall": values.rainfall
-    }])
-    scaled_data = scalar.transform(input_data)
-    prediction = model.predict(scaled_data)
-    decoded_prediction = label_encoder.inverse_transform(prediction)
+    try:
+        input_data = pd.DataFrame([{
+            "N": values.N,
+            "P": values.P,
+            "K": values.K,
+            "temperature": values.temperature,
+            "humidity": values.humidity,
+            "ph": values.ph,
+            "rainfall": values.rainfall
+        }])
+        scaled_data = scalar.transform(input_data)
+        prediction = model.predict(scaled_data)
+        decoded_prediction = label_encoder.inverse_transform(prediction)
+        
+        return {"prediction": decoded_prediction[0]}
+    except KeyError as e:
+        return {"error": f"Missing key in input data: {str(e)}"}, 400
     
-    return {"prediction": decoded_prediction[0]}
+    except ValueError as e:
+        return {"error": f"Value error: {str(e)}"}, 400
+    
+    except AttributeError as e:
+        return {"error": f"Attribute error: {str(e)}. Ensure model and scalar are properly loaded."}, 500
+    
+    except Exception as e:
+        return {"error": f"An unexpected error occurred: {str(e)}"}, 500
+
+
+@app.post('/soil_classification')
+async def soil_classification(file: UploadFile = File(...)):
+    try:
+        file_bytes = await file.read()
+        np_image = np.frombuffer(file_bytes, np.uint8)
+        img = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+        if img is None:
+            return JSONResponse(
+                content={"error": "Failed to decode the image."},
+                status_code=400,
+            )
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+        preprocessed_image = preprocess_image(img)
+        
+        prediction = soil_classifier.predict(preprocessed_image)
+        
+        decoded_prediction = label_encoder_sc.inverse_transform([np.argmax(prediction)])
+        return{'soil': decoded_prediction[0]}
+        
+        
+    except Exception as e:
+        pass
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Crop Prediction API"}
